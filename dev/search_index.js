642839f6e944969ed05b1fb5efdd44767d62787c
Documenter.jl <documenter@juliadocs.github.io>
2021-02-14 21:01:31 +0000

    build based on f568acc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"examples/","page":"Examples","title":"Examples","text":"This is an example where I will put a Jupyter notebook","category":"page"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = MomentClosure","category":"page"},{"location":"#MomentClosure","page":"Home","title":"MomentClosure","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MomentClosure]","category":"page"}]
+}
