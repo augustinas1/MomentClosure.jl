59b709e7f404cffe43d77e377f1964d893d71bba
Augustinas Sukys <augustinas.sukys@gmail.com>
2021-02-18 16:35:49 +0000

    docs skeleton

--- docs/make.jl
+++ docs/make.jl
@@ -7,14 +7,28 @@ makedocs(;
     repo="https://github.com/augustinas1/MomentClosure.jl/blob/{commit}{path}#L{line}",
     sitename="MomentClosure.jl",
     format=Documenter.HTML(;
-        mathengine=Documenter.Writers.HTMLWriter.MathJax(),
+        mathengine=Documenter.Writers.HTMLWriter.MathJax2(),
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://augustinas1.github.io/MomentClosure.jl",
         assets=String[],
     ),
     pages=[
         "Home" => "index.md",
-        "Examples" => "examples.md"
+        "Theory" => Any[
+            "theory/moment_equations.md",
+            "theory/zero_closure.md",
+            "theory/normal_closure.md",
+            "theory/poisson_closure.md"
+        ],
+        "Tutorials" => Any[
+            "tutorials/model_initialisation.md",
+            "tutorials/SSA.md",
+            "tutorials/moment_closure.md",
+            "tutorials/Brusselator.md",
+            "tutorials/P53_system.md",
+            "tutorials/derivative_matching_example.md",
+            "tutorials/gene_network.md"
+        ]
     ],
 )
 
--- docs/src/index.md
+++ docs/src/index.md
@@ -10,3 +10,13 @@ CurrentModule = MomentClosure
 ```@autodocs
 Modules = [MomentClosure]
 ```
+
+## Installation
+MomentClosure can be installed through the Julia package manager:
+
+```julia
+]add https://github.com/augustinas1/MomentClosure.jl
+using MomentClosure
+```
+
+Note the use of full link to github repo as the package is currently unregistered.
