124f8861b77a205d8f07aad57e111cb08ecd18f6
Augustinas Sukys <augustinas.sukys@gmail.com>
2022-04-11 23:15:51 +0100

    update (fix) latexify tests

--- test/latexify_tests.jl
+++ test/latexify_tests.jl
@@ -17,27 +17,19 @@ rn = ReactionSystemMod(t, vars, ps, as, S)
 raw_eqs = generate_raw_moment_eqs(rn, 2)
 closed_raw_eqs = moment_closure(raw_eqs, "conditional gaussian", binary_vars)
 
-# seesm that two terms in the last equation may switch places due to some internal Latexify processing
-expr1 = replace(raw"\begin{align*}
-\frac{d\mu{_{10}}}{dt} =& k_{on} - k_{on} \mu{_{10}} - k_{off} \mu{_{10}}^{-1} \mu{_{11}}^{2} \\
-\frac{d\mu{_{01}}}{dt} =& b k_{p} \mu{_{10}} - \gamma_{p} \mu{_{01}} \\
-\frac{d\mu{_{11}}}{dt} =& k_{on} \mu{_{01}} + b k_{p} \mu{_{10}} - k_{on} \mu{_{11}} - \gamma_{p} \mu{_{11}} - k_{off} \mu{_{10}}^{-2} \mu{_{11}}^{3} \\
-\frac{d\mu{_{02}}}{dt} =& \gamma_{p} \mu{_{01}} + b k_{p} \mu{_{10}} + 2 b k_{p} \mu{_{11}} + 2 k_{p} b^{2} \mu{_{10}} - 2 \gamma_{p} \mu{_{02}}
-\end{align*}
-", "\r\n"=>"\n")
-expr2 = replace(raw"\begin{align*}
-\frac{d\mu{_{10}}}{dt} =& k_{on} - k_{on} \mu{_{10}} - k_{off} \mu{_{10}}^{-1} \mu{_{11}}^{2} \\
-\frac{d\mu{_{01}}}{dt} =& b k_{p} \mu{_{10}} - \gamma_{p} \mu{_{01}} \\
-\frac{d\mu{_{11}}}{dt} =& k_{on} \mu{_{01}} + b k_{p} \mu{_{10}} - k_{on} \mu{_{11}} - \gamma_{p} \mu{_{11}} - k_{off} \mu{_{10}}^{-2} \mu{_{11}}^{3} \\
-\frac{d\mu{_{02}}}{dt} =& \gamma_{p} \mu{_{01}} + b k_{p} \mu{_{10}} + 2 k_{p} b^{2} \mu{_{10}} + 2 b k_{p} \mu{_{11}} - 2 \gamma_{p} \mu{_{02}}
+expr = replace(raw"\begin{align*}
+\frac{d\mu_{1 0}}{dt} =& k_{on} - k_{on} \mu_{1 0} - k_{off} \mu_{1 0}^{-1} \mu_{1 1}^{2} \\
+\frac{d\mu_{0 1}}{dt} =& b k_{p} \mu_{1 0} - \gamma_{p} \mu_{0 1} \\
+\frac{d\mu_{1 1}}{dt} =& k_{on} \mu_{0 1} + b k_{p} \mu_{1 0} - k_{on} \mu_{1 1} - \gamma_{p} \mu_{1 1} - k_{off} \mu_{1 0}^{-2} \mu_{1 1}^{3} \\
+\frac{d\mu_{0 2}}{dt} =& \gamma_{p} \mu_{0 1} + b k_{p} \mu_{1 0} + 2 k_{p} b^{2} \mu_{1 0} + 2 b k_{p} \mu_{1 1} - 2 \gamma_{p} \mu_{0 2}
 \end{align*}
 ", "\r\n"=>"\n")
 exprl = latexify(closed_raw_eqs)
-@test exprl == expr1 || exprl == expr2
+@test exprl == expr
 
 expr = replace(raw"\begin{align*}
-\mu{_{12}} =& \mu{_{10}}^{-1} \mu{_{11}}^{2} \\
-\mu{_{13}} =& 3 \mu{_{10}}^{-1} \mu{_{11}} \mu{_{12}} - 2 \mu{_{10}}^{-2} \mu{_{11}}^{3}
+\mu_{1 2} =& \mu_{1 0}^{-1} \mu_{1 1}^{2} \\
+\mu_{1 3} =& 3 \mu_{1 0}^{-1} \mu_{1 1} \mu_{1 2} - 2 \mu_{1 0}^{-2} \mu_{1 1}^{3}
 \end{align*}
 ", "\r\n"=>"\n")
 @test latexify(closed_raw_eqs, :closure) == expr
