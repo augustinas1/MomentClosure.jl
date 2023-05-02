993217e800e9cef532bd4c50d2a0a0975765ea66
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-05-02 15:30:27 -0400

    Update reaction_system_tests.jl

--- test/reaction_system_tests.jl
+++ test/reaction_system_tests.jl
@@ -11,10 +11,11 @@ S_mat = [ 1 -1  1 -1;
 a = [c₁*X*Y*(X-1)/Ω^2, c₂*X, c₃*Ω, c₄*X]
 
 rn = @reaction_network begin
+    @parameters c₁ c₂ c₃ c₄ Ω
     (c₁/Ω^2), 2X + Y → 3X
     (c₂), X → Y
     (Ω*c₃, c₄), 0 ↔ X
-end c₁ c₂ c₃ c₄ Ω
+end
 smap = speciesmap(rn)
 
 @test isequal(get_stoichiometry(rn, smap), S_mat)
