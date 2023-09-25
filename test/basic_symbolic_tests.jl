5b4f8edd4e56d4b2a7214df77fe2c4abdb37af75
Augustinas Sukys <augustinas.sukys@gmail.com>
2023-09-25 01:47:16 +0100

    fix tests
    simple symbolic expression reordering

--- test/basic_symbolic_tests.jl
+++ test/basic_symbolic_tests.jl
@@ -23,11 +23,15 @@ fcs, pwrs, mpwr = polynomial_propensities([expr], t, smap)
 
 expr = x*(x^2+y) / (c+2)
 fcs, pwrs, mpwr = polynomial_propensities([expand(expr)], t, smap)
-@test all(isequal.(fcs[1], [1 / (2 + c), 1 / (2 + c)])) && pwrs[1] == [[3, 0], [1, 1]] && mpwr == 3
+@test mpwr == 3 && 
+    all(isequal.(fcs[1], [1 / (2 + c), 1 / (2 + c)])) &&
+    ( pwrs[1] == [[3, 0], [1, 1]] || pwrs[1] == [[1, 1], [3, 0]] )
 
 expr = c^2*x + y / c
 fcs, pwrs, mpwr = polynomial_propensities([expr], t, smap)
-@test all(isequal.(fcs[1], [c^2, 1 / c])) && pwrs[1] == [[1, 0], [0, 1]] && mpwr == 1
+@test mpwr == 1 &&
+    ( all(isequal.(fcs[1], [1 / c, c^2])) || all(isequal.(fcs[1], [c^2, 1 / c])) ) && 
+    ( pwrs[1] == [[0, 1], [1, 0]] || pwrs[1] == [[1, 0], [0, 1]] )
 
 expr = x / (y+1) 
 @test_throws ErrorException polynomial_propensities([expr], t, smap)
--- test/latexify_tests.jl
+++ test/latexify_tests.jl
@@ -21,34 +21,21 @@ clean_eqs = bernoulli_moment_eqs(raw_eqs, binary_vars)
 closed_raw_eqs = moment_closure(raw_eqs, "conditional gaussian", binary_vars)
 
 # latexify output is very sensitive to Julia version and latexify + Symbolics updates...
-# NOTE: implementing a lazy test to avoid that
-#=
-expr = replace(raw"\begin{align*}
-\frac{d\mu_{1 0}}{dt} =& k_{on} - k_{on} \mu_{1 0} - k_{off} \mu_{1 2} \\
-\frac{d\mu_{0 1}}{dt} =& k_{p} p^{-1} \mu_{1 0} - \gamma_{p} \mu_{0 1} - k_{p} \mu_{1 0} \\
-\frac{d\mu_{1 1}}{dt} =& k_{on} \mu_{0 1} + k_{p} p^{-1} \mu_{1 0} - k_{p} \mu_{1 0} - k_{off} \mu_{1 3} - k_{on} \mu_{1 1} - \gamma_{p} \mu_{1 1} \\
-\frac{d\mu_{0 2}}{dt} =& \gamma_{p} \mu_{0 1} + k_{p} \mu_{1 0} + 2 k_{p} p^{-2} \mu_{1 0} + 2 k_{p} p^{-1} \mu_{1 1} - 2 k_{p} \mu_{1 1} - 2 \gamma_{p} \mu_{0 2} - 3 k_{p} p^{-1} \mu_{1 0}
-\end{align*}
-", "\r\n"=>"\n")
-exprl = latexify(clean_eqs)
-@test exprl == expr1 || exprl == expr2
-=#
 expr = replace(raw"\begin{align*}
 \frac{d\mu_{1 0}}{dt} =& k_{on}", "\r\n"=>"\n")
 @test latexify(clean_eqs)[1:46] == expr
 
-#=
-expr = replace(raw"\begin{align*}
-\mu_{1 2} =& \mu_{1 0}^{-1} \mu_{1 1}^{2} \\
-\mu_{1 3} =& 3 \mu_{1 0}^{-1} \mu_{1 1} \mu_{1 2} - 2 \mu_{1 0}^{-2} \mu_{1 1}^{3}
+expr1 = replace(raw"\begin{align*}
+\mu_{1 2} =& \frac{\mu_{1 1}^{2}}{\mu_{1 0}} \\
+\mu_{1 3} =& \frac{3 \mu_{1 2} \mu_{1 1}}{\mu_{1 0}} + \frac{-2 \mu_{1 1}^{3}}{\mu_{1 0}^{2}}
 \end{align*}
 ", "\r\n"=>"\n")
-=#
-expr = replace(raw"\begin{align*}
+expr2 = replace(raw"\begin{align*}
 \mu_{1 2} =& \frac{\mu_{1 1}^{2}}{\mu_{1 0}} \\
 \mu_{1 3} =& \frac{-2 \mu_{1 1}^{3}}{\mu_{1 0}^{2}} + \frac{3 \mu_{1 1} \mu_{1 2}}{\mu_{1 0}}
 \end{align*}
 ", "\r\n"=>"\n")
-@test latexify(closed_raw_eqs, :closure) == expr
+exprl = latexify(closed_raw_eqs, :closure)
+@test (exprl == expr1) || (exprl == expr2)
 
 @test_throws MethodError latexify(raw_eqs, :closure)
